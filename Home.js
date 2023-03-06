240cc4a40c69bfd3360911be841004e841136cb1
BehnamEsmaili <58373858+Behnamesmaili94@users.noreply.github.com>
2023-03-06 16:34:55 +0330

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+
+    <body>
+        
+        <h1 class="text">welcome</h1>
+        <br>
+        
+       
+        <script src="loops.js"></script>
+    </body>
+</html>
